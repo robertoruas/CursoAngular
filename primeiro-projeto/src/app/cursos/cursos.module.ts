a01c44109b7c0f678251a94604e1230cd998c856
Roberto Ruas <roberto.ruasm@gmail.com>
2017-11-19 10:24:33 -0200

    Aula 7

--- primeiro-projeto/src/app/cursos/cursos.module.ts
+++ primeiro-projeto/src/app/cursos/cursos.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CursosComponent } from './cursos.component';
 import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
+import { CursosService } from './cursos.service';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [CursosComponent, CursoDetalheComponent],
-  exports: [
-    CursosComponent
-  ]
+  exports: [CursosComponent],
+  providers: [CursosService]
 })
 export class CursosModule { }
