64437ca7d9c1e909056bb8a8400c1463a62484df
Roberto Ruas <roberto.ruasm@gmail.com>
2017-12-05 22:44:05 -0200

    Aula 15

--- data-binding/src/app/app.component.ts
+++ data-binding/src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  valor: number = 5;
+  deletarCiclo: Boolean = false;
+  mudarValor(){
+    this.valor++;
+  }
+
+  destruirCiclo(){
+    this.deletarCiclo = true;
+  }
 }
--- data-binding/src/app/app.module.ts
+++ data-binding/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { DataBindingComponent } from './data-binding/data-binding.component';
 import { MeuFormModule } from './meu-form/meu-form.module';
 import { InputPropertyComponent } from './input-property/input-property.component';
 import { OutputPropertyComponent } from './output-property/output-property.component';
+import { CicloComponent } from './ciclo/ciclo.component';
 
 
 @NgModule({
@@ -15,7 +16,8 @@ import { OutputPropertyComponent } from './output-property/output-property.compo
     AppComponent,
     DataBindingComponent,
     InputPropertyComponent,
-    OutputPropertyComponent
+    OutputPropertyComponent,
+    CicloComponent
   ],
   imports: [
     BrowserModule,
